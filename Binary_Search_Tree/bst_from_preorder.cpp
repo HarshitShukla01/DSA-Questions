ea001a49c5dff1885c77fa17400ff7d3b76d7191
HarshitShukla01 <harshitshukla258@gmail.com>
2021-08-22 20:05:40 +0530

    bst from preorder started

---
+++ Binary_Search_Tree/bst_from_preorder.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    Node *left,*right;
+    Node(int value)
+    {
+        data=value;
+        right=left=NULL;
+    }
+};
+
+void  printBST(Node* root)
+{
+   if(root==NULL) return;
+   printBST(root->left);
+   cout<<root->data<<" ";
+   printBST(root->right);
+}
+
+int main()
+{
+
+  Node* root = new Node(4);
+  root->left = new Node(2);
+  root->right = new Node(5);
+  root->left->left = new Node(1);
+  root->left->right = new Node(3);
+  root->right->right = new Node(6);
+
+
+}
