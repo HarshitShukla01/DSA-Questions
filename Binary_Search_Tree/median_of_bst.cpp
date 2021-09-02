85e4f806e94aa42e184f97bb360ed48bca133701
HarshitShukla01 <harshitshukla258@gmail.com>
2021-09-02 18:12:22 +0530

    median of bst - file modified

--- Binary_Search_Tree/median_of_bst.cpp
+++ Binary_Search_Tree/median_of_bst.cpp
@@ -19,7 +19,7 @@ void  printBST(Node* root)
    printBST(root->right);
 }
 
-void traverse_bst(Node* root,int &n)
+void traverse_bst(Node* root,int& n)
 {
    if(root==NULL) return;
    traverse_bst(root->left,n);
@@ -27,32 +27,36 @@ void traverse_bst(Node* root,int &n)
    traverse_bst(root->right,n);
 }
 
-void odd_find(Node* root,int i,int& j,int& med)
+void odd_find(Node* root,int i,int& j,double& med)
 {
    if(root==NULL) return;
    odd_find(root->left,i,j,med);
-   if(i==j) {med=root->data; return;}
+   if(i==j) {med=(double)root->data; j++; return;}
    j++;
    odd_find(root->right,i,j,med);
 }
 
-void even_find(Node* root,int i,int& j,int& med)
+void even_find(Node* root,int i,int& j,double& med)
 {
    if(root==NULL) return;
    even_find(root->left,i,j,med);
-   if(i==j) {med=root->data;}
-   if(i+1==j) {med=(med+root->data)/2; return;}
+   
+   if(i==j) {med=(double)root->data;}
+   else if(i+1==j) {med=(med+(double)root->data)/2; j++;return;}
+
    j++;
    even_find(root->right,i,j,med);
 }
 
-int find_median(Node* root)
+double find_median(Node* root)
 {
     if(root==NULL) return -1;
     int n=0;
     traverse_bst(root,n);
+
     int i=n/2,j=0;
-    if(n/2==0) even_find(root,i,j,med);
+    double med=0;
+    if(n%2==0) {j++;even_find(root,i,j,med);}
     else odd_find(root,i,j,med);
 
     return med;
@@ -62,11 +66,11 @@ int main()
 {
 
   Node* root = new Node(4);
-  root->left = new Node(1);
+  root->left = new Node(2);
   root->right = new Node(5);
-  root->left->left = new Node(2);
+  root->left->left = new Node(1);
   root->left->right = new Node(3);
-  root->right->right = new Node(6);
+  //root->right->right = new Node(6);
 
   cout<<"BST : ";
   printBST(root);
