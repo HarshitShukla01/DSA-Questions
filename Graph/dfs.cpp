412afae68e1d704a3f37340bf85bab42e01b5827
HarshitShukla01 <harshitshukla258@gmail.com>
2021-09-10 10:15:18 +0530

    dfs

---
+++ Graph/dfs.cpp
@@ -0,0 +1,106 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define vi vector<int>
+#define vvi vector<vector<int> >
+#define pii pair<int,int>
+#define vpi vector<pair<int,int>>
+#define u_map unordered_map   
+#define vcIT vector<int>::iterator 
+
+void input_adjancey(vi adj[],int n)
+{
+    for(int i=1;i<=n;i++)
+    {
+        int st,end;
+        cin>>st>>end;
+        adj[st].push_back(end);
+        adj[end].push_back(st);
+    }
+}
+void output_adjancey(vi adj[],int n)
+{
+    cout<<"Adjancey list:: "<<endl;
+   for(int i=1;i<=n;i++)
+    {
+    cout<<i<<" -> ";
+    vcIT it;
+     for(it=adj[i].begin();it!=adj[i].end();it++)
+     {
+         cout<<*it<<" ";
+     }
+     cout<<endl;
+    }
+}
+
+const int N =100005;
+bool vis[N];
+
+void fillValue()
+{
+    for(int i=0;i<N;i++)
+    vis[i]=true;
+}
+
+
+vector<int> ret;
+
+void solve(int n,map<int,int>& mp,vector<int>adj[])
+{
+    mp[n]=1;
+    ret.push_back(n);
+     for(auto it=0;it<adj[n].size();it++)
+     {
+         int val = adj[n][it];
+         if(mp.find(val)==mp.end()) 
+         {
+            solve(val,mp,adj);
+         }
+     }
+    for(auto x:adj[n])
+    {
+        if(!mp[x])
+        solve(x,mp,adj);
+    }
+}
+
+vector<int> dfs_traversal(vector<int> adj[],int n)
+{
+  map<int,int> mp;
+  solve(1,mp,adj);
+  return ret;
+}
+
+int main()
+{
+    int n; //n = no of nodes 
+    cout<<"Number of Nodes = ";
+    cin>>n;
+
+    vi adj[n+1]; //2-D vector of elements filled zero initaly
+    
+    input_adjancey(adj,n);
+   // output_adjancey(adj,n);
+
+    // fillValue();   //use this or use map
+    
+    vi arr = dfs_traversal(adj,n);
+
+    cout<<"dfs = ";
+    for(int i=0;i<arr.size();i++)
+    cout<<arr[i]<<" ";
+  
+}
+
+
+/*
+7  
+1 2
+1 3
+2 4
+2 5
+2 6
+2 7
+7 3
+*/
