79d1fc93f82a465a7ca05f08dda96cef7a51c295
HarshitShukla01 <harshitshukla258@gmail.com>
2021-09-21 19:40:15 +0530

    cycle detection using dfs in directed

---
+++ Graph/cylce_in_directed.cpp
@@ -0,0 +1,67 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define vi vector<int>
+#define vvi vector<vector<int> >
+#define pii pair<int,int>
+#define vpi vector<pair<int,int>>
+#define u_map unordered_map   
+#define vcIT vector<int>::iterator 
+
+
+bool isCycle(int index,vector<vector<int>> adj,vector<int>& st_arr,vector<bool>& vis)
+{
+    st_arr[index]=1;
+
+    if(vis[index]==false);
+    {
+        vis[index]=true;
+
+        for(auto i:adj[index])
+        {
+            if(!vis[i]&&isCycle(i,adj,st_arr,vis)) return true;
+            if(st_arr[i]==1) return true;
+        }
+    }
+
+    st_arr[index]=0;
+    return false;
+}
+
+
+
+void find_cycle(int n,vector<vector<int>> adj)
+{
+   vector<int> st_arr(n,0);
+   vector<bool> vis(n,0);
+   bool cycle=false;
+
+   for(int i=1;i<=n;i++)
+    {
+        if(!vis[i]&&isCycle(i,adj,st_arr,vis))
+        cycle=true;
+    }
+
+    if(cycle) cout<<"cycle is present";
+    else cout<<"cycle is not present";
+}
+
+int main()
+{
+    int n,m; 
+    cout<<"Number of Nodes = ";
+    cin>>n;
+    cout<<"Number of Edges = ";
+    cin>>m;
+    vector<vector<int>> adj(n+1);
+
+    for(int i=0;i<m;i++)
+    {
+        int r,t;
+        cin>>r>>t;
+        adj[r].push_back(t);
+    }
+
+    find_cycle(n,adj);
+}
