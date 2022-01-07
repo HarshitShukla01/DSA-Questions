6a7102f978bd801712727c68ae8d93873e5f7a8b
Harshit Shukla <harshitshukla258@gmail.com>
2022-01-07 12:18:16 +0530

    kruskal algo done

--- Graph/kruskal_algo.cpp
+++ Graph/kruskal_algo.cpp
@@ -1,9 +1,42 @@
 #include<bits/stdc++.h>
 using namespace std;
+#define big_size 1e5+6
 
+vector<int> parent(big_size);
+vector<int> sz(big_size);
+
+int find_set(int v){
+    if(v==parent[v]) return v;
+
+    return parent[v] = find_set(parent[v]);
+}
+
+void union_set(int x,int y){
+    int a = find_set(x);
+    int b = find_set(y);
+
+    if(a!=b){
+        if(sz[a]<sz[b]) swap(a,b);
+
+        parent[b]=a;
+        sz[a]+=sz[b];
+    }
+}
+
+void make_set(int v){
+    parent[v] = v;
+    sz[v] = v;
+}
+
+bool sortcol( const vector<int>& v1,const vector<int>& v2 ) {
+ return v1[2] < v2[2];
+}
 
 int main()
 {
+    for(int i=0;i<big_size;i++)
+        make_set(i);
+
     int n,m;
     cin>>n>>m;
     vector<vector<int>> edges;
@@ -16,30 +49,27 @@ int main()
     }
 
     sort(edges.begin(), edges.end(),sortcol);
-  
-    int src = 0;
-    vector<int> dist(n,INF);
-
-
-    dist[edges[0][0]] = 0;
-    dist[edges[0][1]] = edges[0][2];
+    
+    int src=0;
+    int sum = 0;
     for(auto e: edges)
     {
         int u=e[0],v=e[1],w=e[2];
-        dist[v]=min(dist[v],w);
+        int x = find_set(u), y = find_set(v);
+        if(x==y) continue;
+        else{
+            union_set(x,y);
+            sum+=w;
+        }
+    }
 
-    }
+    cout<<sum;
    
-    int sum=0;
-    for(int i=0;i<n;i++)
-    {
-        cout<<"i = "<<i<<" , dist = "<<dist[i]<<endl;
-        sum=sum+dist[i];
-    }
-    cout<<"sum = "<<sum;
+    
 }
 
 /*
+4 5
 0 1 10
 0 2 6
 0 3 5
