e6c14dd2ddd59992be68ae331a641843ec4223c2
Harshit Shukla <harshitshukla258@gmail.com>
2022-03-15 20:33:16 +0530

    catlan number cp

---
+++ Dynamic_Programming/catlan_number.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int catlan_number(int n)
+{
+	// formulaa => C(n+1) => summation of (Ci * C(n-i)) where i is from 0 to n
+	// example => C4 = C(3+1) => C0*C3 + C1*C2 + C2*C1 + C3*C0
+	 
+	vector<int> dp(n+1,0);
+	dp[0]=dp[1]=1;
+
+	for(int i=2;i<=n;i++)
+		for(int j=0;j<i;j++)
+			dp[i] += (dp[j]*dp[i-j-1]); 
+
+
+	return dp[n];
+}
+
+int main(){
+
+    int n;
+    cin>>n;
+    
+    cout<<catlan_number(n)<<endl;
+
+} 
